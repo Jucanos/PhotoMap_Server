9131314d3f29686e5cae668efcc8424c09b5c610
RunaticMoon <mwngh3@gmail.com>
2020-01-05 18:02:30 +0900

    [기능] Access Token 인증 구현
    stateless의 session유지를 위하여 Access Token을 검증하는 코드 추가
    jwt를 이용하여 api gateway에서 policy를 확인한다.

---
+++ src/auth.js
@@ -0,0 +1,62 @@
+// AWS xray 연결
+const awsXRay = require('aws-xray-sdk');
+const awsSdk = awsXRay.captureAWS(require('aws-sdk'));
+
+// JWT 가져오기
+const jwt = require('jsonwebtoken');
+
+// 인증정보 받아오기
+exports.refresh = async (event, ctx) => {
+  // TODO: JWT Refresh 구현
+  return createResponse(200, { message: 'refresh' });
+};
+
+// Policy helper function
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+
+// Reusable Authorizer function, set on `authorizer` field in serverless.yml
+module.exports.verify = (event, context, callback) => {
+  if (!event.authorizationToken) {
+    return callback('Unauthorized');
+  }
+
+  const tokenParts = event.authorizationToken.split(' ');
+  const tokenValue = tokenParts[1];
+
+  if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
+    // no auth token!
+    return callback('Unauthorized');
+  }
+
+  try {
+    jwt.verify(tokenValue, process.env.JWT_SECRET, (verifyError, decoded) => {
+      if (verifyError) {
+        console.log('verifyError', verifyError);
+        // 401 Unauthorized
+        console.log(`Token invalid. ${verifyError}`);
+        return callback('Unauthorized');
+      }
+      // is custom authorizer function
+      console.log('valid from customAuthorizer', decoded);
+      return callback(null, generatePolicy('user', 'Allow', event.methodArn));
+    });
+  } catch (err) {
+    console.log('catch error. Invalid token', err);
+    return callback('Unauthorized');
+  }
+};
