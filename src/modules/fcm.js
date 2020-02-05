5fef4a4ce993ffff0c5e12313ee4099db8b7656a
RunaticMoon <mwngh3@gmail.com>
2020-02-05 16:49:47 +0900

    [기타] Slack Notification 추가

--- src/modules/fcm.js
+++ src/modules/fcm.js
@@ -7,7 +7,7 @@ if (admin.apps.length == 0) {
       type: 'service_account',
       project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gi, '\n'),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: 'https://accounts.google.com/o/oauth2/auth',
